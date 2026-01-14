604eee838f860386de315ef2d12e6ac60d70e14b
Vinícius Lima <viniciusgabrielss10@gmail.com>
2026-01-14 19:09:02 -0300

    inicial

---
+++ src/main/java/com/ViniGBPl/biblioteca/BibliotecaApiApplication.java
@@ -0,0 +1,13 @@
+package com.ViniGBPl.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaApiApplication.class, args);
+	}
+
+}
