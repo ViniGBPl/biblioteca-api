a82bfedcb58041fa6e1e5f7e5198fdaad2ec2469
Vinícius Lima <viniciusgabrielss10@gmail.com>
2026-01-19 03:19:21 -0300

    finalizado a busca

--- src/main/java/com/ViniGBPl/biblioteca/domain/service/LivroService.java
+++ src/main/java/com/ViniGBPl/biblioteca/domain/service/LivroService.java
@@ -6,6 +6,8 @@ import com.ViniGBPl.biblioteca.domain.repository.LivroRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.ViniGBPl.biblioteca.domain.model.LivroDocument;
+import com.ViniGBPl.biblioteca.domain.repository.LivroSearchRepository;
 
 @Service
 @RequiredArgsConstructor
@@ -13,14 +15,26 @@ public class LivroService {
 
     private final LivroRepository livroRepository;
     private final AutorService autorService;
+    private final LivroSearchRepository livroSearchRepository; // Adicione esta linha
 
     @Transactional
     public Livro salvar(Livro livro) {
-        // Para cadastrar um livro, o autor deve existir
         Long autorId = livro.getAutor().getId();
         Autor autor = autorService.buscarOurFalhar(autorId);
-
         livro.setAutor(autor);
-        return livroRepository.save(livro);
+
+        Livro livroSalvo = livroRepository.save(livro);
+
+        // Sincroniza com Elasticsearch
+        LivroDocument document = LivroDocument.builder()
+                .id(livroSalvo.getId().toString())
+                .titulo(livroSalvo.getTitulo())
+                .autorNome(autor.getNome())
+                .isbn(livroSalvo.getIsbn())
+                .build();
+
+        livroSearchRepository.save(document);
+
+        return livroSalvo;
     }
 }
